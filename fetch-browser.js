714908fe698f235b5f581a0e1a994db7c6f55df8
John Shearar <john.shearar@gmail.com>
2016-06-22 14:50:56 +0200

    Expose various globals so that whatwg-fetch's support object works as expected

--- fetch-browser.js
+++ fetch-browser.js
@@ -4,9 +4,20 @@
   function fetchPonyfill(options) {
     var Promise = options && options.Promise || self.Promise;
     var XMLHttpRequest = options && options.XMLHttpRequest || self.XMLHttpRequest;
+    var URLSearchParams = options && options.URLSearchParams || self.URLSearchParams;
+    var FormData = options && options.FormData || self.FormData;
+    var Blob = options && options.Blob || self.Blob;
+    var ArrayBuffer = options && options.ArrayBuffer || self.ArrayBuffer;
+    var Symbol = options && options.Symbol || self.Symbol;
 
     return (function () {
       var self = {};
+      self.URLSearchParams = URLSearchParams;
+      self.FormData = FormData;
+      self.FileReader = FileReader;
+      self.Blob = Blob;
+      self.ArrayBuffer = ArrayBuffer;
+      self.Symbol = Symbol;
 
 // {{whatwgFetch}}
 
